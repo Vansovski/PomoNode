e3e7c14ef1642081afc4e71601da2755e0f355bd
Marcelo Vansovski de Melo <marcelo_vansovski@hotmail.com>
2024-12-12 17:10:29 -0300

    Service de CRUD para todos

--- backend/src/app/todo/todo.service.ts
+++ backend/src/app/todo/todo.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TodoEntity } from './entities/todo.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(
+    @InjectRepository(TodoEntity)
+    private readonly todoRepository: Repository<TodoEntity>,
+  ) {}
+
+  async findAll() {
+    return await this.todoRepository.find();
+  }
+
+  async findOne(id: string) {
+    try {
+      return await this.todoRepository.findOneOrFail({ where: { id } });
+    } catch (error) {
+      throw new NotFoundException(
+        `Todo with ID "${id}" not found ` + error.message,
+      );
+    }
+  }
+  async create(data) {
+    return await this.todoRepository.save(this.todoRepository.create(data));
+  }
+
+  async update(id: string, data) {
+    const todo = await this.findOne(id);
+    this.todoRepository.merge(todo, data);
+
+    return await this.todoRepository.save(todo);
+  }
+
+  async deleteById(id: string) {
+    await this.findOne(id);
+
+    await this.todoRepository.softDelete(id);
+  }
+}
