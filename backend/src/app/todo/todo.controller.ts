3827da68c3c951fd48b738e214721247a8ed90be
Marcelo Vansovski de Melo <marcelo_vansovski@hotmail.com>
2024-12-12 17:24:16 -0300

    Controller

--- backend/src/app/todo/todo.controller.ts
+++ backend/src/app/todo/todo.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { TodoService } from './todo.service';
 
 @Controller('api/v1/todos')
-export class TodoController {}
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Get()
+  async getall() {
+    return await this.todoService.findAll();
+  }
+
+  @Post()
+  async create(@Body() body) {
+    return await this.todoService.create(body);
+  }
+
+  @Get(':id')
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    return await this.todoService.findOne(id);
+  }
+
+  @Put(':id')
+  async update(@Param('id', new ParseUUIDPipe()) id: string, @Body() body) {
+    return await this.todoService.update(id, body);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id', new ParseUUIDPipe()) id: string) {
+    await this.todoService.deleteById(id);
+  }
+}
